211e8347f535a5c5233c6d0bcd141c0d0988fae5
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-06-13 16:06:45 +0300

    Add project files.

---
+++ Ex36-37-LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Specialized;
+
+namespace Ex36_37_LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
