bf6537b9229c38cb4c320fe85d1ebbe1dd54213b
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-06-15 15:15:21 +0300

    Ex36-Øvelse2.3 klargøring

---
+++ ADT/LinkedList.cs
@@ -0,0 +1,18 @@
+﻿namespace ADT
+{
+    public class LinkedList
+    {
+        private class Node
+        {
+            public object Data { get; set; }
+            public Node Next { get; set; }
+
+            public override string ToString()
+            {
+                return Data.ToString();
+            }
+
+        }
+
+    }
+}
